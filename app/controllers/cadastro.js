bfb674df6e9f34e284933f2ed56ccb524071cabd
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-16 15:04:18 -0300

    receiving and validating the data

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,23 @@
 module.exports.cadastro = function (application, req, res){
-    res.render('cadastro')
+    res.render('cadastro', {validacao: {}, dadosForm: {}})
+}
+module.exports.cadastrar = function (application, req, res){  
+
+    const dadosForm = req.body;
+
+    req.assert('nome', 'nome não pode ser vazio').notEmpty();
+    req.assert('usuario', 'usuario não pode ser vazio').notEmpty();
+    req.assert('senha', 'senha não pode ser vazio').notEmpty();
+    req.assert('casa', 'casa não pode ser vazio').notEmpty();
+
+    const erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+        return;
+    }
+    else{
+        res.send("pronto")
+        return;
+    }
 }
