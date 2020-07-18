c19256e0d8bc78ff46496b8d45faef4de0d5888c
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-18 09:51:22 -0300

    created the controller of autentication

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,18 @@
 module.exports.index = function(application, req, res){
-	res.render('index');
+	res.render('index', {validacao: {}});
+}
+
+module.exports.autenticar = function(application, req, res){
+	const dadosForm = req.body
+
+	req.assert("usuario", "Usuario não pode ser vazio!").notEmpty()
+	req.assert("senha", "Senha não pode ser vazio!").notEmpty()
+
+	const erros = req.validationErrors()
+
+	if(erros){
+		return res.render('index', {validacao: erros})
+	}
+
+	return res.send("deu certo")
 }
