ebde76688477689394752c08f537476db206b06c
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-18 13:05:29 -0300

    adjust the project

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -7,3 +7,11 @@ module.exports.jogo = function(application, req, res){
 		res.send('Usuário precisa fazer login')
 	}
 }
+
+module.exports.sair = function(application, req, res){
+
+	req.session.destroy(function(){
+		//res.render("index", {validacao: {}})
+		res.redirect('/')
+	})
+}
